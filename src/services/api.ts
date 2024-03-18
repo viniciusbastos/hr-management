63f6f3f2d23e441dbbff36b2688aa3f92f190b87
Vinicius Bastos <vinicius.bastos@me.com>
2024-03-18 10:49:35 -0300

    hell

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const api = axios.create({
-  baseURL: "https://api-hr-manager-6a1f85c2202b.herokuapp.com/",
+  baseURL: "https://hr-manager-backend.onrender.com/api",
   headers: {
     Authorization: `Bearer ${token}`,
   },
