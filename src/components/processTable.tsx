9d5e9c9f5705b6c66e54fd6b9339e1cb7fbfbb86
Vinicius Bastos <vinnibastos@gmail.com>
2024-06-12 09:50:06 -0300

    add process inventory table update

--- src/components/processTable.tsx
+++ src/components/processTable.tsx
@@ -5,7 +5,6 @@ interface Process {
   id: number;
   name: string;
   description: string;
-  status: string;
   frequency: string;
   user: {
     name: string;
@@ -19,30 +18,63 @@ const ProcessTable: React.FC = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Process Inventory</h1>
-      <table className="min-w-full bg-white border">
-        <thead>
+      <h1 className="text-2xl font-bold mb-4">Inventário de Processos</h1>
+      <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg"></div>
+      <table className="min-w-full bg-white border  divide-y divide-gray-200 dark:divide-gray-700">
+        <thead className="bg-gray-50 dark:bg-gray-800">
           <tr>
-            <th className="py-2 px-4 border">Process Name</th>
+            <th className="py-2 px-4 border">Nome do Process</th>
             <th className="py-2 px-4 border">Description</th>
-            <th className="py-2 px-4 border">Status</th>
-            <th className="py-2 px-4 border">Frequency</th>
-            <th className="py-2 px-4 border">Responsible User</th>
+            <th className="py-2 px-4 border">Frequencia</th>
+            <th className="py-2 px-4 border">Responsável</th>
           </tr>
         </thead>
         <tbody>
-          {processes.map((process) => (
-            <tr key={process.id}>
+            <tr key="1">
               <td className="py-2 px-4 border">Boletim Interno Ostensivo</td>
-              <td className="py-2 px-4 border"></td>
               <td className="py-2 px-4 border"></td>
               <td className="py-2 px-4 border">Mensal</td>
               <td className="py-2 px-4 border">SD PM MANOEL</td>
             </tr>
-          ))}
+            <tr key="2">
+              <td className="py-2 px-4 border">Mapa de Substituição de Função</td>
+              <td className="py-2 px-4 border"></td>
+              <td className="py-2 px-4 border">Quadrimestral</td>
+              <td className="py-2 px-4 border">Cap PM VINICIUS</td>
+            </tr>
+            <tr key="3">
+              <td className="py-2 px-4 border">Mapa de Condutores - CET</td>
+              <td className="py-2 px-4 border"></td>
+              <td className="py-2 px-4 border">Quadrimestral</td>
+              <td className="py-2 px-4 border">Cap PM VINICIUS</td>
+            </tr>
+            <tr key="4">
+              <td className="py-2 px-4 border">Plano de Férias</td>
+              <td className="py-2 px-4 border"></td>
+              <td className="py-2 px-4 border">Anual</td>
+              <td className="py-2 px-4 border">Cap PM VINICIUS / SGT PM ELISANGELA</td>
+            </tr>
+            <tr key="5">
+              <td className="py-2 px-4 border">Memorando de Férias</td>
+              <td className="py-2 px-4 border"></td>
+              <td className="py-2 px-4 border">Mensal</td>
+              <td className="py-2 px-4 border">SGT PM ELISANGELA</td>
+            </tr>
+            <tr key="5">
+              <td className="py-2 px-4 border">Atualização do Péculio</td>
+              <td className="py-2 px-4 border"></td>
+              <td className="py-2 px-4 border">Sempre que houver movimentação</td>
+              <td className="py-2 px-4 border">CAP PM VINICIUS</td>
+            </tr>
+            <tr key="5">
+              <td className="py-2 px-4 border">Requisições para Apresentação à Justiça</td>
+              <td className="py-2 px-4 border"></td>
+              <td className="py-2 px-4 border">Sempre que houver requisição</td>
+              <td className="py-2 px-4 border">ST PM RR JULIO CESAR - Func. Civil Janne</td>
+            </tr>
         </tbody>
       </table>
-    </div>
+      </div>
   );
 };
 
