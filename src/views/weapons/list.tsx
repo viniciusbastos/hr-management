a2858bf71dc36d8d634ab68e8aa976c309d2e913
Vinicius <vinicius.bastos@me.com>
2025-07-12 23:03:56 -0300

    changs

--- src/views/weapons/list.tsx
+++ src/views/weapons/list.tsx
@@ -440,7 +440,7 @@ const WeaponsList = () => {
 
                       <PDFDownloadLink
                         document={<WeaponsPDF weapon={weapon} />}
-                        fileName={`weapon-${weapon.id}-report.pdf`}
+                        fileName={`requerimento-${weapon.id}.pdf`}
                       >
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ">
                           <>
@@ -456,7 +456,7 @@ const WeaponsList = () => {
                       </PDFDownloadLink>
                       <PDFDownloadLink
                         document={<ResponsabilityTermPDF weapon={weapon} />}
-                        fileName={`weapon-${weapon.id}-report.pdf`}
+                        fileName={`termo-responsabilidade-${weapon.id}.pdf`}
                       >
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ">
                           <>
