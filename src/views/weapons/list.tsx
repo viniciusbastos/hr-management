203c54dd5d2e26b27a9ba9c760f7cff026019b80
Vinicius <vinicius.bastos@me.com>
2025-07-30 12:03:13 -0300

    add cadastrar armamento mudanca in button

--- src/views/weapons/list.tsx
+++ src/views/weapons/list.tsx
@@ -20,11 +20,12 @@ import { api } from '../../services/api'
 import FormWeapon from '../../components/formWeapon'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { toast, ToastContainer } from 'react-toastify'
-import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer'
+import { PDFDownloadLink, PDFViewer, pdf } from '@react-pdf/renderer'
 import WeaponsPDF from './weaponChargePdf'
 import CardDashboard from '../../components/cardDashboard'
 import ResponsabilityTermPDF from './termoResponsabilidade'
 import { useUsers } from '../../App'
+import { saveAs } from 'file-saver'
 
 /**
  * Component for displaying and managing a list of weapons.
@@ -120,6 +121,10 @@ const WeaponsList = () => {
       console.error('Error deleting weapons:', error)
       // Handle error (e.g., show an error message to the user)
     }
+  }
+  const handleDownloadPDF = async (weapon: Weapon) => {
+    const blob = await pdf(<WeaponsPDF weapon={weapon} />).toBlob()
+    saveAs(blob, `requerimento-${weapon.id}.pdf`)
   }
 
   const handleInputChange = (e) => {
@@ -443,22 +448,20 @@ const WeaponsList = () => {
                         </svg>
                       </button>
 
-                      <PDFDownloadLink
-                        document={<WeaponsPDF weapon={weapon} />}
-                        fileName={`requerimento-${weapon.id}.pdf`}
+                      <button
+                        onClick={() => handleDownloadPDF(weapon)}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded "
                       >
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ">
-                          <>
-                            <svg
-                              className="w-5 h-5"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                            >
-                              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                            </svg>
-                          </>
-                        </button>
-                      </PDFDownloadLink>
+                        <>
+                          <svg
+                            className="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                          >
+                            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                          </svg>
+                        </>
+                      </button>
 
                       <PDFDownloadLink
                         document={<ResponsabilityTermPDF weapon={weapon} />}
